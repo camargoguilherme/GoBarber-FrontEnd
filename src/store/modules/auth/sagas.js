b1d2d8d423966e9cfb5961b3c3acb9e09da0a7c9
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2020-04-16 12:39:58 -0300

    Adicionado notificações com react-toastify

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 
 import history from '~/services/history';
 import api from '~/services/api';
@@ -16,13 +17,15 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('Usuario não é prestador');
+      toast.error('Usuario não é prestador');
+      yield put(signFailure());
       return;
     }
     yield put(signInSuccess(token, user));
 
     history.push('/dashboard');
   } catch (error) {
+    toast.error('Falha na autenticação, verifique seus dados');
     yield put(signFailure());
   }
 }
