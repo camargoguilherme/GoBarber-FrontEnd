c3c1e19e6ac2efe0b36d4b7bed9b73bed808f73d
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2020-04-15 21:56:39 -0300

    Adicionada configurações para os forms

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+import { Form } from '@unform/web';
+import Input from '~/components/Input';
+
 import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+    alert(JSON.stringify(data));
+  }
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input placeholder="Nome Completo" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
