79de44cfb8b099910a6ac45c96580a1cbe2a72e4
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2020-04-15 22:47:14 -0300

    Adicionada validações dos inputs do form

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
-import { Form } from '@unform/web';
-import Input from '~/components/Input';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '~/assets/logo.svg';
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email('Insira um e-mail válido')
+    .required('O e-mail é obrigatório'),
+  password: Yup.string().required('A senha é obrigatória'),
+});
 
 export default function SignIn() {
   function handleSubmit(data) {
     console.tron.log(data);
-    alert(JSON.stringify(data));
   }
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <Form onSubmit={handleSubmit}>
+      <Form schema={schema} onSubmit={handleSubmit}>
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input
           name="password"
