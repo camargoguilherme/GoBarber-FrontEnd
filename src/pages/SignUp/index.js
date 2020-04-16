0d87c53d78f01ae2a9a30ff3dbf5b78325a00bf5
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2020-04-15 21:20:14 -0300

    Adicionada estilização a pagina de Login

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,18 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import logo from '~/assets/logo.svg';
 
 export default function SignUp() {
-  return <h1>Cadastro</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <form>
+        <input placeholder="Nome Completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
