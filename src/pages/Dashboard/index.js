5fe4612b8d4f5c42fa006e2fb9eafb623d1f007a
Guilherme Camargo <guilhermecamargo57@hotmail.com>
2020-05-02 02:25:21 -0300

    Adicionada estilizaçao da tela de dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,41 @@
 import React from 'react';
 import api from '~/services/api';
-// import { Container } from './styles';
+
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#FFF" />
+        </button>
+        <strong>01 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#FFF" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Guilherme Camargo</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time available>
+          <strong>10:00</strong>
+          <span>William Maia</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Eduardo Quintanilha</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
